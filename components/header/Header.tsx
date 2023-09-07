a9e0cc299c0364182c533f37100378b55048d981
이상윤 <zaar625@gmail.com>
2023-09-07 13:35:42 +0900

    header_ product navbar added

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,57 +1,52 @@
 'use client'
 
-import React from 'react';
+import React,{useState} from 'react';
 import { usePathname } from 'next/navigation'
 import Link from 'next/link';
-import {AiOutlineMenu} from 'react-icons/ai'
+import {AiOutlineMenu,AiOutlineClose} from 'react-icons/ai'
+import { headerNav , productNav} from '@/constant/navigation';
 
 import './header.scss';
 
 
 export default function Header() {
-
-  const headerNav = [
-    {
-      display: 'login',
-      path:'/login',
-    },
-    {
-      display:'like',
-      path:'/like'
-    },
-    {
-      display:'mypage',
-      path:'/mypage'
-    },
-    {
-      display:'cart',
-      path:'/cart'
-    },
-  ];
+  const [menuIsActive, setMenuIsActive] = useState(false) 
 
   const pathName = usePathname();
-  console.log(pathName)
-  
 
   return (
     <div className='header'>
-      <div className='header__wrap container'>
-        <p className='logo'>BABAN</p>   
-        <div className='menu'>
-          <AiOutlineMenu/>    
+      <div className='header__wrap'>
+        <div className='header__top container'>
+          <p className='logo'>BABAN</p>   
+          <div className='menu' onClick={()=>setMenuIsActive(!menuIsActive) }>
+          {menuIsActive ? <AiOutlineClose/> : <AiOutlineMenu/> }   
+          </div>
+          
+          <ul className='header__nav-right'>
+          {headerNav.map((link, index) => {
+          const isActive = pathName === link.path;
+          return (
+            <li>
+              <Link  href={link.path} key={link.display} className={`${isActive ? 'active' : ''}`}>{link.display}</Link>
+            </li>
+          )
+        })}
+          </ul>
         </div>
-        
-        <ul className='header__nav-right'>
-        {headerNav.map((link, index) => {
-        const isActive = pathName === link.path;
-        return (
-          <li>
-            <Link  href={link.path} key={link.display} className={`${isActive ? 'active' : ''}`}>{link.display}</Link>
-          </li>
-        )
-      })}
-        </ul>
+          {menuIsActive &&(
+            <ul className='product-nav'>
+              {productNav.map((link, index) => {
+              return (
+                <li>
+                  <Link href={'/'} key={link.display}>{link.display}</Link>
+                </li>
+              )
+            })}
+          </ul>) 
+        }
       </div>
+     
     </div>
   )
 }
---
+++ constant/navigation.ts
@@ -0,0 +1,30 @@
+export const headerNav = [
+    {
+      display: 'login',
+      path:'/login',
+    },
+    {
+      display:'like',
+      path:'/like'
+    },
+    {
+      display:'mypage',
+      path:'/mypage'
+    },
+    {
+      display:'cart',
+      path:'/cart'
+    },
+  ];
+//MARCE CELANO BELL NUOVO FLOUI LUCIO BRUNI CASUAL
+export const productNav = [
+   { display:'MARCE'},
+   { display:'CELANO'},
+   { display:'BELL'},
+   { display:'NUOVO'},
+   { display:'FLOUI'},
+   { display:'LUCIO'},
+   { display:'BRUNI'},
+   { display:'CASUAL'},
+
+]
