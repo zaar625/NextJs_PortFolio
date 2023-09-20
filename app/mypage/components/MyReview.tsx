9150d4a5f73138cdae86c70e91b98a6684ed204d
이상윤 <zaar625@gmail.com>
2023-09-20 20:27:38 +0900

    마이페이지_구매내역 및 작성한 글 데이터 가져오기

--- app/mypage/components/MyReview.tsx
+++ app/mypage/components/MyReview.tsx
@@ -3,6 +3,7 @@
 import React, {useEffect, useState} from 'react'
 import { collection,query,where,getDocs,DocumentData,deleteDoc,doc } from 'firebase/firestore';
 import { db } from '@/lib/firebaseConfig';
+import '../mypage.scss';
 import Link from 'next/link';
 
 export default function MyReview({user}:{user:string | null}) {
@@ -25,34 +26,42 @@ export default function MyReview({user}:{user:string | null}) {
 
    async function deletReview(refId:string) {
       if (!confirm("게시물을 삭세하시겠습니까?")) {
-          // 취소(아니오) 버튼 클릭 시 이벤트
           return;
       } else {
         await deleteDoc(doc(db, "review",refId)).then(()=> alert('해당 리뷰 게시물이 삭제되었습니다.'));
         getMyReview();
       }
     }
-
+    console.log(reviewData)
   return (
-    <div>
-        {
-           reviewData && reviewData.map((reviewItem, index) => (
-            <div key={index}>
-                <div>{reviewItem.title}</div>
-                <button onClick={() => deletReview(reviewItem.id)}>삭제</button>
-                <br/>
-                <Link href={{
-                    pathname:'/mypage/editReview',
-                    query:{
-                      id:reviewItem.id, //문서 아이디
-                      content:reviewItem.content,
-                      title:reviewItem.title,
-                      class:reviewItem.class,
-                    }
-                    }}>수정</Link>
+    <div className='myReview'>
+      <h1 className='mb-1'>작성한 글</h1>
+      {
+        reviewData ? reviewData.map((reviewItem, index) => (
+          <div key={index} className='myReview__card '>
+            <div className='myReview__card__product'>
+              <p>상품: {reviewItem.product}</p>
+              <p>제목: {reviewItem.title}</p>
             </div>
-           )) 
-        }
+            <div className='myReview__card__btnWrap'>
+              <button onClick={() => deletReview(reviewItem.id)}>삭제</button>
+              <Link href={{
+                  pathname:'/mypage/editReview',
+                  query:{
+                    id:reviewItem.id, //문서 아이디
+                    content:reviewItem.content,
+                    title:reviewItem.title,
+                    class:reviewItem.class,
+                  }
+              }}>수정</Link>
+            </div>
+          </div>
+        ))
+        :
+        <div>
+          <p>작성한 리뷰가 없습니다.</p>
+        </div>
+      }
     </div>
   )
 }
--- app/mypage/components/Mypurchase.tsx
+++ app/mypage/components/Mypurchase.tsx
@@ -3,6 +3,9 @@
 import React, { useEffect, useState } from 'react'
 import { db } from '@/lib/firebaseConfig';
 import { collection ,query, where, getDocs, DocumentData} from 'firebase/firestore';
+import '../mypage.scss';
+import format from 'date-fns/format';
+import Image from 'next/image';
 
 export default function Mypurchase({user}:{user:string | null}) {
   const [purchaseItem, setPurchaseItem] = useState<DocumentData[]>();
@@ -17,11 +20,29 @@ export default function Mypurchase({user}:{user:string | null}) {
     setPurchaseItem(purchases);
   };
 
-  // console.log(purchaseItem)
+  // console.log(purchaseItem);
+
   useEffect(()=>{
     getUserPurchaseItem();
-  },[])
+  },[user]);
+
   return (
-    <div>Mypurchase</div>
+    <div className='purchase mb-2'>
+      <h1>구매 내역</h1>
+      {
+        purchaseItem ? purchaseItem.map((item) => (
+          <div key={item.orderId} className='purchase__card'>
+            <p>주문아이디: {item.orderId}</p>
+            <p>상품명: {item.orderName}</p>
+            <p>총 결제 금액: {item.totalAmount}</p>
+            <p>결제 일시: {format(new Date(item.approvedAt), "yyyy/MM/dd HH:mm:ss")}</p>
+          </div>
+        ))
+        :
+        <div>
+          <p>구매 내역이 없습니다.</p>
+        </div>
+      }
+    </div>
   )
 }
