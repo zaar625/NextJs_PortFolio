c6fca9328f71fdf8a1527bbc5d00d97ddc849a7f
이상윤 <zaar625@gmail.com>
2023-09-27 10:51:30 +0900

    무한로딩 error edit

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,37 +1,12 @@
 'use client'
 
 import React,{useState,useEffect,useRef ,useCallback} from 'react';
-<<<<<<< HEAD
-import { usePathname } from 'next/navigation'
-=======
->>>>>>> d6ce32d (무한로딩 error edit)
 import Link from 'next/link';
 import {AiOutlineMenu,AiOutlineClose} from 'react-icons/ai'
 import { productNav} from '@/constant/navigation';
 import { useSession,signOut } from "next-auth/react"
 import {auth} from '@/lib/firebaseConfig'
 import { onAuthStateChanged } from 'firebase/auth';
-<<<<<<< HEAD
-import { doc, getDoc,onSnapshot } from 'firebase/firestore';
-import { db } from '@/lib/firebaseConfig';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/redux/store';
-
-import './header.scss';
-
-
-
-export default function Header() {
-  const [menuIsActive, setMenuIsActive] = useState(false);
-  const [firebaseUser, setFirebaseUser] = useState<null | string>(null);
-  const { data: snsSession } = useSession();
-  const nonUserCartItems = useSelector((state: RootState) => state.cartItem.items);
-  const [cartLength, setCartLength] = useState(0);
-
-  const headerRef = useRef<HTMLDivElement>(null);
-
-  const isUser = firebaseUser || snsSession?.user?.name
-=======
 import { doc,onSnapshot } from 'firebase/firestore';
 import { db } from '@/lib/firebaseConfig';
 import { useSelector } from 'react-redux';
@@ -48,9 +23,6 @@ export default function Header() {
   const headerRef = useRef<HTMLDivElement>(null);
 
   const isUser = firebaseUser || snsSession?.user?.name;
-
-  console.log(nonUserCartItems)
->>>>>>> d6ce32d (무한로딩 error edit)
 
   useEffect(() => {
     const shrinkHeader = () => {
@@ -88,37 +60,6 @@ export default function Header() {
     }
   }
 
-<<<<<<< HEAD
-  // 파이어베이스 로그인 상태 확인
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
-      const uid = user.uid;
-      setFirebaseUser(user.uid)
-      // ...
-    } else {
-      setFirebaseUser(null);
-      // User is signed out
-      // ...
-    }
-  });
-
-  const getCartItem = useCallback(async () => {
-    if(isUser) {
-      onSnapshot(doc(db, "user", isUser), (doc) => {
-        console.log('추가')
-        const data = doc.data();
-        setCartLength(data?.cart.length);
-    });
-    
-      // const docRef = doc(db, "user",isUser);
-      // const docSnap = await getDoc(docRef); 
-      // const data = docSnap.data();
-      
-    } else {
-      setCartLength(nonUserCartItems.length);
-=======
   useEffect(()=>{
     // 파이어베이스 로그인 상태 확인
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -143,19 +84,12 @@ export default function Header() {
         const data = doc.data();
         setCartLength(data?.cart.length);
       });
->>>>>>> d6ce32d (무한로딩 error edit)
     }
   },[isUser]);
 
   useEffect(()=>{
-<<<<<<< HEAD
-    getCartItem();
-  },[snsSession,firebaseUser,nonUserCartItems,getCartItem])
-
-=======
     getUserCartItem();
   },[isUser])
->>>>>>> d6ce32d (무한로딩 error edit)
 
   return (
     <header ref ={headerRef}className='header'>
@@ -177,11 +111,10 @@ export default function Header() {
               {isUser && <Link href={'/mypage'}>mypage</Link>}
             </li>
             <li>
-<<<<<<< HEAD
-              <Link href={'/cart'}>{`cart(${cartLength})`}</Link>
-=======
-              <Link href={'/cart'}>{`cart(${isUser ? cartLength : nonUserCartItems.length})`}</Link>
->>>>>>> d6ce32d (무한로딩 error edit)
+              {
+                isUser ?  <Link href={'/cart'}>{`cart(${cartLength})`}</Link> :  <Link href={'/cart'}>{`cart(${nonUserCartItems.length})`}</Link>
+              }
+             
             </li>
           </ul>
         </div>
