8de6d610a237120af9da118133b9791f3a9b8298
이상윤 <zaar625@gmail.com>
2023-11-10 14:26:36 +0900

    test: product detail - count test

--- components/productDetail/tests/ProductDesc.test.tsx
+++ components/productDetail/tests/ProductDesc.test.tsx
@@ -1,5 +1,6 @@
 import ProductDesc from '../ProductDesc';
-import { render } from '@/utils/test-utils/renderWithContext';
+import { render, screen } from '@/utils/test-utils/renderWithContext';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('next/navigation', () => ({
   useRouter() {
@@ -24,13 +25,80 @@ jest.mock('next-auth/react', () => {
   };
 });
 
-const products = [
+const mockProducts = [
   {
-    name: 'hello',
-    color: ['white']
+    name: 'Nicke Chain Shoulder Bag',
+    color: ['white', 'black']
   }
 ];
 
-test('상품에 대한 설명', () => {
-  render(<ProductDesc product={products} />);
+test('상품에 대한 설명이 있어야합니다.', async () => {
+  render(<ProductDesc product={mockProducts} />);
+
+  const productTitle = await screen.findByRole('heading');
+  const figcaptionElement = screen.getByTestId('custom-figcaption');
+
+  expect(productTitle).toBeInTheDocument();
+  expect(figcaptionElement).toBeInTheDocument();
 });
+
+test('사용자가 색상 버튼클 클릭하면 해당 색상이 active됩니다.', async () => {
+  const user = userEvent.setup();
+  render(<ProductDesc product={mockProducts} />);
+
+  const colorBtnElement = await screen.findByRole('button', { name: 'white' });
+
+  await user.click(colorBtnElement);
+
+  expect(colorBtnElement).toHaveClass('color_active');
+});
+
+test('수량의 초기 값은 1입니다.', async () => {
+  const initialNum = 1;
+  const user = userEvent.setup();
+  const { container } = render(<ProductDesc product={mockProducts} />);
+
+  const quantityEl = container.querySelector('.productDetail__des__quan');
+  const pElementInsideColor = quantityEl?.querySelector('p');
+
+  expect(pElementInsideColor).toHaveTextContent(`${initialNum}`);
+});
+
+test('수량의 마이너스 아이콘을 클릭하면 현재 수량의 -1이됩니다.', async () => {
+  const user = userEvent.setup();
+  const { container } = render(<ProductDesc product={mockProducts} />);
+
+  const quantityEl = container.querySelector('.productDetail__des__quan');
+  const pElementValue = quantityEl?.querySelector('p');
+  const minusIcon = container.querySelector('.minus-icon') as Element;
+
+  if (pElementValue?.textContent === '1') {
+    await user.click(minusIcon);
+    expect(pElementValue).toHaveTextContent('1');
+
+    return;
+  }
+
+  if (Number(pElementValue?.textContent) === 2) {
+    await user.click(minusIcon);
+    expect(pElementValue).toHaveTextContent('1');
+  }
+});
+
+test('수량의 플러스 아이콘을 클릭하면 현재 수량의 +1이 됩니다.', async () => {
+  const user = userEvent.setup();
+  const { container } = render(<ProductDesc product={mockProducts} />);
+
+  const quantityEl = container.querySelector('.productDetail__des__quan');
+  const pElementValue = quantityEl?.querySelector('p');
+  const plusIcon = container.querySelector('.plus-icon') as Element;
+
+  if (pElementValue?.textContent === '1') {
+    await user.click(plusIcon);
+    expect(pElementValue).toHaveTextContent('2');
+
+    return;
+  }
+});
+
+test('카트에 담기 버튼과 구매하기 버튼이 있습니다.', () => {});
